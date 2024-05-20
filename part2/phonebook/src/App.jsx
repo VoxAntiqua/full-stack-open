bf2e4bd42f7bef4a0259e2895768b97558d87cc8
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-05-20 15:08:57 -0400

    Implemented adding entries to db.json, fixed infinitely firing useEffect

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     axios.get("http://localhost:3001/persons").then((response) => {
       setPersons(response.data);
     });
-  });
+  }, []);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -34,11 +34,14 @@ const App = () => {
       const personObject = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1,
       };
-      setPersons(persons.concat(personObject));
-      setNewName("");
-      setNewNumber("");
+      axios
+        .post("http://localhost:3001/persons", personObject)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+          setNewName("");
+          setNewNumber("");
+        });
       console.log(newNumber);
     }
   };

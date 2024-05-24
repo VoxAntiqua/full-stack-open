f4edca6d210dbcc7ad99f008b158aa7e8a46017f
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-05-24 08:27:17 -0400

    implemented display limit

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,4 +1,7 @@
 const Countries = ({ countries }) => {
+  if (countries.length > 10) {
+    return <div>Too many matches, specify another filter</div>;
+  }
   return (
     <ul>
       {countries.map((c) => (

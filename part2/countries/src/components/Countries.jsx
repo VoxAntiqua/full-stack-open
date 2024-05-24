a1246c3deb90f535c73fb4da57e4713464c9f43d
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-05-24 08:39:41 -0400

    Implemented country data

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,6 +1,10 @@
+import CountryData from "./CountryData";
+
 const Countries = ({ countries }) => {
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>;
+  } else if (countries.length === 1) {
+    return <CountryData country={countries[0]} />;
   }
   return (
     <ul>
---
+++ part2/countries/src/components/CountryData.jsx
@@ -0,0 +1,18 @@
+const CountryData = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>capital {country.capital[0]}</div>
+      <div>area {country.area}</div>
+      <h3>languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} />
+    </div>
+  );
+};
+
+export default CountryData;
